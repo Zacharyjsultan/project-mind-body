c8df3bf2164f54dc764a054d441aa096a4919ac4
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-10-17 14:47:47 -0700

    journal inputs built

---
+++ src/Components/Mind/Journal/Journal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Journal() {
+  return (
+    <>
+      <div>
+        <h1>Journal</h1>
+        <label>I am grateful for...</label>
+        <input></input>
+      </div>
+      <div>
+        <label>What would make today great?</label>
+        <input></input>
+      </div>
+      <div>
+        <label>Daily affirmation. I am...</label>
+        <input></input>
+      </div>
+      <div>
+        <label>Three amazing things that happened today...</label>
+        <input></input>
+      </div>
+      <div>
+        <label>How could I have made today even better?</label>
+        <input></input>
+      </div>
+    </>
+  );
+}

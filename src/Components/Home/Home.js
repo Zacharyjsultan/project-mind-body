280e38b567bf7f7aa925f31f29658faab87acfa1
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-10-18 11:21:43 -0700

    nav flow works better

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import Body from '../Body/Body';
+import Header from '../Header/Header';
+
+
 import Mind from '../Mind/Mind';
+import Productivity from '../Productivity/Productivity';
 import './Home.css';
 
 export default function Home() {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
+
   return <div className="home-main">
-    <Mind />
+    <Header />
+    <Productivity />
+    <Switch>
+      <Route path="/home/mind" component={Mind} />
+      <Route path="/home/body" component={Body} />
+    </Switch>
+    
   </div>;
 }

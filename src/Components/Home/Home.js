36193a7cf60d472c12518d3db01a882c7041e576
Kyle McCall <kylem147@gmail.com>
2022-10-17 18:00:59 -0700

    signout. not fully functional

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
 import './Home.css';
 
 export default function Home() {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    <Redirect to="/auth-sign-in" />;
+  }
+
   return <div className="home-main">Home</div>;
 }
--- src/Components/Productivity/Productivity.js
+++ src/Components/Productivity/Productivity.js
@@ -5,10 +5,21 @@ import Todos from '../ToDos/Todos';
 import useTodos from '../../hooks/useTodos';
 import Pomodoro from '../Pomodoro/Pomodoro';
 import { Button } from '@mui/material';
+import { signOut } from '../../services/auth';
+import { Redirect } from 'react-router-dom';
 
 export default function Productivity() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const { todos, setTodos } = useTodos();
+
+  const handleLogout = async () => {
+    await signOut();
+    setUser(null);
+  };
+
+  if (!user) {
+    <Redirect to="/auth-sign-in" />;
+  }
 
   return (
     <div className="productivity-main">
@@ -23,7 +34,7 @@ export default function Productivity() {
       </div>
       <div className="signout">
         <img src={require('./logout.png')} className="logout-icon" />
-        <Button variant="outlined" className="signout-btn">
+        <Button variant="outlined" className="signout-btn" onClick={handleLogout}>
           Signout
         </Button>
       </div>

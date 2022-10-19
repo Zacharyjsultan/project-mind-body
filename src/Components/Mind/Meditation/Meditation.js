8145982d0b06aca189ca7b59981cdcb5c660f349
Kyle McCall <kylem147@gmail.com>
2022-10-18 17:49:48 -0700

    adds meditation animation

--- src/Components/Mind/Meditation/Meditation.js
+++ src/Components/Mind/Meditation/Meditation.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import './Meditation.css';
 
 export default function Meditation() {
-  return <div className="meditation-body"></div>;
+  return (
+    <div className="meditation-body">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }

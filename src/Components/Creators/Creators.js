2a43b6d129635bd62b8bd84bbd83d862c530db72
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-19 12:49:12 -0700

    creators page

--- src/Components/Creators/Creators.js
+++ src/Components/Creators/Creators.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 export default function Creators() {
   return (
-    <div className='creator-container'>
-        Creators
-      
-
+    <div>
+      <div className="creators-container">
+        <div className="creators">
+          <p className="andrew">
+            Andrew
+            <img src={require('./andrew.jpg')} />
+            https://www.linkedin.com/in/andrewjamesboyle/ https://github.com/andrewjamesboyle
+          </p>
+          <p className="zach">
+            Zach https://www.linkedin.com/in/zachary-sultan/ https://github.com/Zacharyjsultan
+          </p>
+          <p className="charlie">
+            Charlie
+            <img src={require('./charlie.png')} />
+          </p>
+          <p className="kyle">
+            Kyle
+            <img src={require('./me.jpeg')} />
+            https://www.linkedin.com/in/kyle-mccall/ https://github.com/kyle-j-mccall
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

7bce8c87de39a17322d22431949e8ca3a4598dc7
Kyle McCall <kylem147@gmail.com>
2022-10-19 13:02:59 -0700

    stretches css

--- src/Components/Body/Stretches/Stretches.js
+++ src/Components/Body/Stretches/Stretches.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
 import { fetchStretches } from '../../../services/stretches';
+import TextField from '@mui/material/TextField';
+import './Stretches.css';
+import { Button } from '@mui/material';
 
 export default function Stretches() {
   const [stretches, setStretches] = useState([]);
   const [search, setSearch] = useState('');
+  console.log(stretches.instructions);
 
   const searchStretches = async () => {
     const resp = await fetchStretches(search);
@@ -12,13 +16,29 @@ export default function Stretches() {
   };
 
   return (
-    <div>
-      <label>Search for a stretch</label>  
-      <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      {stretches.map((stretch, idx) => (
-        <div key={idx}>{stretch.name}{stretch.instructions}</div>
-      ))}
-      <button onClick={searchStretches}>Search</button>
+    <div className="stretches-body">
+      <div className="stretches-input">
+        <TextField
+          id="outlined-basic"
+          label="Search stretches"
+          variant="outlined"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="stretch-search"
+        />
+
+        <Button onClick={searchStretches} className="stretch-btn" variant="outlined">
+          Search
+        </Button>
+      </div>
+      <div className="stretches-container">
+        {stretches.map((stretch, idx) => (
+          <div key={idx} className="stretches-container">
+            <h3>{stretch.name}</h3>
+            <p>{stretch.instructions}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
